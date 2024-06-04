5c7a842475c394a32f21f1940ba590be1eb37be9
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-06-04 16:05:47 -0300

    continuando tela de logim e cadastro

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,9 +1,16 @@
 function verificarSenha(){  
+    const form = document.getElementById('form')
     const senha = document.getElementById('senha').value
     const confirmeSenha = document.getElementById('confirme-senha').value
+    const inputDeSenha = document.getElementById('password-box')
+    const senhaIncorretaTexto = document.getElementById('wrong-password-text')
+    const minimoDeCaracters = document.getElementById('min-characters')
     
     if (senha != confirmeSenha){
-        alert('Senhas diferentes')
-    }
+        inputDeSenha.classList.add('wrong-password')
+        senhaIncorretaTexto.innerHTML = `<p class="wrong-password-text">Senhas diferentes</p>`
+        form.addEventListener('submit', function(evento) {
+            evento.preventDefault();
+        });
+   }
 }
-
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,7 +2,7 @@ function logar(){
     const email = document.getElementById('email').value
     const senha = document.getElementById('password').value
 
-    if(email == 'admin@gmail.com' && senha == 'admin'){
+    if(email == 'admin@gmail.com' && senha == '123'){
         alert('logado!')
     } else{
         alert('nao logado!')
