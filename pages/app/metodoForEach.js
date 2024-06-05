378a3509f72af9d924bba39502a73e9e2f94ea08
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-06-05 15:11:20 -0300

    array de vídeos

--- pages/app/metodoForEach.js
+++ pages/app/metodoForEach.js
@@ -5,13 +5,13 @@ function exibirOsVideosNaTela(listaDeVideos){
   listaDeVideos.forEach(video => {
 
     elementoParaInserirVideos.innerHTML += ` <div class="video">
-    <iframe class='video-yt' src="${video.imagem}" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" frameborder="0" allowfullscreen/> </iframe>
+    <iframe class='video-yt' src="${video.link}" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" frameborder="0" allowfullscreen/> </iframe>
 
     <h2 class="video__titulo">
-     <a href="${video.imagem}">${video.titulo}</a>
+     <a href="${video.link}">${video.titulo}</a>
     </h2>
 
-    <p class="video__descricao">${video.autor}</p>
+    <p class="video__descricao">${video.canal}</p>
 
     <div class="tags">
       <span class="tag">${video.categoria}</span>
--- pages/app/metodoSort.js
+++
@@ -1,9 +0,0 @@
-let btnOrdenarPorTitulo = document.getElementById('btnOrdenarPorTitulo')
-
-btnOrdenarPorTitulo.addEventListener('click', btnOrdenarPorTitulo)
-
-function btnOrdenarPorTitulo(){
-  let VideosOrdenados = videos.sort((a, b)=> a.preco - b.preco)
-  exibirOsLivrosNaTela(VideosOrdenados)
-}
-
